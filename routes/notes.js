cd85fe81b4f82c0155e9f0dea6369798ede13771
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-08-22 22:19:20 +0200

    Fix route to notes

--- routes/notes.js
+++ routes/notes.js
@@ -47,7 +47,7 @@ notes.delete('/:note_id', (req, res) => {
 
 
 // The POST route for a new note
-notes.post('/notes', (req, res) => {
+notes.post('/api/notes', (req, res) => {
   console.log(req.body);
 
   const { title, text } = req.body;
